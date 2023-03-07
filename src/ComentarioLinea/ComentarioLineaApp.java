b9fb02b6963029618eed53862bdf9096fee7ea1f
yamir <yamir@mandragora.mandragora>
2023-03-07 12:16:32 +0100

    First commit

---
+++ src/ComentarioLinea/ComentarioLineaApp.java
@@ -0,0 +1,12 @@
+package ComentarioLinea;
+
+
+public class ComentarioLineaApp {
+
+	public static void main(String[] args) {
+		// System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+
+}
